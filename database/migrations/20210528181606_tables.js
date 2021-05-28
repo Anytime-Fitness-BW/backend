c8f323ea8631110173fa82403069a6a23797d1e8
BrittanyCanty <bcanty92@gmail.com>
2021-05-28 18:29:46 -0500

    redo

--- database/migrations/20210528181606_tables.js
+++ database/migrations/20210528181606_tables.js
@@ -1,8 +1,37 @@
-
 exports.up = function(knex) {
+    return (knex.schema.createTable('users', (users) => {
+        users.increments();
   
-};
+        users
+          .string('username', 128)
+          .notNullable()
+          .unique();
 
-exports.down = function(knex) {
+        users
+          .string('password', 128)
+          .notNullable();
   
-};
+        users
+          .boolean("admin")
+          .notNullable()
+          .defaultTo(0)
+    })
+      .createTable('classes', (classes) => {
+          classes.increments();
+          classes.string("name", 128);
+          classes.string("type", 128);
+          classes.string("startTime", 128);
+          classes.string("duration", 128);
+          classes.int("intensity");
+          classes.string("location", 128);
+          classes.integer("numberOfRegisteredAttendees");
+          classes.integer("maxClassSize");
+      }))
+  };
+
+  exports.down = function(knex) {
+      return( knex.schema
+          .dropTableIfExists("classes")
+          .dropTableIfExists("users")
+      )
+  };
---
+++ database/seeds/02-classes.js
@@ -0,0 +1,29 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex('classes').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('classes').insert([
+        {
+          name: "Yoga",
+          type: "stretching",
+          startTime: "2:00pm",
+          duration: "1 hour",
+          intensity: 2,
+          location: "park",
+          numberOfRegisteredAttendees: 7,
+          maxClassSize: 12
+      },
+      {
+          name: "Pilates",
+          type: "cardio",
+          startTime: "1:00pm",
+          duration: "1 hour",
+          intensity: 3,
+          location: "mall",
+          numberOfRegisteredAttendees: 11,
+          maxClassSize: 20
+      },
+      ]);
+    });
+};
